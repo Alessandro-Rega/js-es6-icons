b3e3f96d780d50d746cf0993dcab9fb886b3c89e
Alessandro-Rega <regaale47@gmail.com>
2021-11-23 18:22:38 +0100

    insert script creazione card

--- js/script.js
+++ js/script.js
@@ -113,3 +113,39 @@ const icone = [
 	}
 ];
 
+const cont = document.querySelector(".container");
+createCard("all");
+        // name: 'cat',
+		// prefix: 'fa-',
+		// type: 'animal',
+		// family: 'fas',
+		// color: 'orange'
+
+const filtro = document.getElementById("tipo");
+
+filtro.addEventListener('change', function(){
+    const selezione = document.getElementById("tipo").value;
+    createCard(selezione);
+});
+
+function createCard(selezione){
+    console.log(selezione);
+    cont.innerHTML = "";
+    for(let i = 0; i < icone.length; i++){
+        if(selezione == "all"){
+            cont.innerHTML += 
+            `<div class="card">
+                <i style="color:${icone[i].color};" class="${icone[i].family} ${icone[i].prefix}${icone[i].name}"></i>
+                <span>${icone[i].name}</span>
+            </div>`
+        }
+        else if(icone[i].type == selezione){
+            cont.innerHTML += 
+            `<div class="card">
+                <i style="color:${icone[i].color};" class="${icone[i].family} ${icone[i].prefix}${icone[i].name}"></i>
+                <span>${icone[i].name}</span>
+            </div>`
+        }
+        else i++;
+    }
+}
